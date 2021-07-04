e43a0abe8a792df804c69fabe3362a8827f41ce5
Yash Panchal <72609549+panchaly75@users.noreply.github.com>
2021-07-04 09:29:37 +0530

    Variable, Data types, Operators and Strings

---
+++ project01.py
@@ -0,0 +1,20 @@
+#01 write a python program which accepts the radius of a circle from the user and computes area.
+
+print("\tMyCaptain Python project's : 01\t")
+print("______________________________________________\n")
+
+pi = 3.141592653589793
+
+r = float(input("The radius of the circle : "))
+
+area = pi*r**2
+
+print(f"The area of the circle with radius {r} is : {area}")
+
+
+'''
+I/p
+The radius of the circle : 1.1
+O/p
+The area of the circle with radius 1.1 is: 3.8013271108436504
+'''
